51fcef026f8b7d4bad5772b7347eb8f5d3bfd574
James Afshar-Ritter <james@Jamess-MacBook-Pro.local>
2025-04-16 20:33:30 -0700

    update

--- vm-setup.sh
+++ vm-setup.sh
@@ -14,7 +14,15 @@ echo "Creating a new user '${username}'..."
 useradd -m -s /bin/bash "${username}"
 echo "${username}:${password}" | chpasswd
 usermod -aG sudo "${username}"
-echo "'${username}' user created and added to sudo group."
+echo "User '${username}' user created and added to sudo group."
+
+# Update system packages
+echo "Updating system packages..."
+apt-get update -y
+apt-get dist-upgrade -y
+echo "Installing required packages... (curl, git, ca-certificates)"
+apt-get install -y install -y curl git ca-certificates
+echo "System packages updated."
 
 # Set up GitHub SSH configuration
 read -p "Enter the GitHub username for '${username}': " github_username
@@ -27,12 +35,14 @@ if [ -z "${github_email}" ]; then
   github_email="${username}@localhost"
   echo "Using '${username}@localhost' as GitHub email."
 fi
-echo "Setting up GitHub SSH configuration for '${username}' user..."
+echo "Setting up GitHub SSH configuration for '${username}' user using the password provided..."
 sudo -u "${username}" bash <<EOF
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
-ssh-keygen -t ed25519 -C "${username}@localhost" -f ~/.ssh/id_ed25519 -N ""
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+chmod 644 ~/.ssh/known_hosts
 eval "$(ssh-agent -s)"
+ssh-keygen -t ed25519 -C "${username}@localhost" -f ~/.ssh/id_ed25519 -N ""
 ssh-add ~/.ssh/id_ed25519
 chmod 600 ~/.ssh/id_ed25519
 chmod 644 ~/.ssh/id_ed25519.pub
@@ -44,11 +54,15 @@ echo "Add this key to your GitHub account."
 git config --global user.name "${github_username}"
 git config --global user.email "${github_email}"
 git config --global core.editor "nano"
+git config --global init.defaultBranch main
+git config --global pull.rebase false
+git config --global core.fileMode true
+git config --global http.sslVerify true
+git config --global protocol.version 2
 EOF
 
 # Prepare for install scripts
 echo "Preparing environment for install scripts..."
-apt-get install -y curl git
 echo "Environment ready. Switch to '${username}' user to run install scripts."
 
 echo "Setup complete. Please log in as '${username}' to continue."
